e56c33cfb3291e74ffbae13a4d788376b21ec06e
wejdan <wejdanqt@gmail.com>
2019-10-14 19:47:34 +0300

    “my_commit”

---
+++ MongoDB/Modularization/Modularizing_the_Controllers/server/config/routes.js
@@ -0,0 +1,21 @@
+
+    const mongoose = require('mongoose'),
+    User = mongoose.model('User')
+
+    var user = require('../controllers/user.js');
+
+   module.exports = function(app){
+ 
+    app.get('/', (req, res) => {
+        user.index(req, res);
+    }); 
+
+    app.post('/quote', (req, res) => {
+
+        user.act(req, res);
+    })
+
+    app.get('/quotes', (req, res) => {
+        user.act2(req, res);
+    });
+   }
