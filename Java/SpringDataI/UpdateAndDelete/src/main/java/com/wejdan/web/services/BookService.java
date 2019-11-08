6dd69556870beff84a58ca805549eaf04a3d95c4
wejdan <wejdanqt@gmail.com>
2019-11-09 01:13:13 +0300

    “SpringData git commit -m “SpringData

---
+++ Java/SpringDataI/UpdateAndDelete/src/main/java/com/wejdan/web/services/BookService.java
@@ -0,0 +1,61 @@
+package com.wejdan.web.services;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.wejdan.web.models.Book;
+import com.wejdan.web.repositories.BookRepository;
+
+import java.util.List;
+
+@Service
+public class BookService {
+	 private final BookRepository bookRepository;
+	 
+	 public BookService(BookRepository bookRepository) {
+	        this.bookRepository = bookRepository;
+	    }
+	    // returns all the books
+	    public List<Book> allBooks() {
+	        return bookRepository.findAll();
+	    }
+	    // creates a book
+	    public Book createBook(Book b) {
+	        return bookRepository.save(b);
+	    }
+	    
+      public Book updateBook( Long id  , String title ,  String desc ,  String lang, Integer numOfPages ) {
+    	Optional<Book> optionalBook = bookRepository.findById(id);
+    	  if(optionalBook.isPresent()) {
+	            Book book =  optionalBook.get();
+	            book.setTitle(title);
+	            book.setDescription(desc);
+	            book.setLanguage(lang);
+	            book.setNumberOfPages(numOfPages);
+	            return bookRepository.save(book);
+	            
+	        } else {
+	            return null;
+	        }
+ 
+	    }
+	    
+	   public void deleteBook(Long id) {
+		   bookRepository.deleteById(id);
+        }
+	    
+	    
+	    // retrieves a book
+	    public Book findBook(Long id) {
+	        Optional<Book> optionalBook = bookRepository.findById(id);
+	        if(optionalBook.isPresent()) {
+	            return optionalBook.get();
+	        } else {
+	            return null;
+	        }
+	    }
+	
+	
+
+}
